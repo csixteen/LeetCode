3d952b4e1145858b298ae55e68ef1f6c87318fb4
Pedro Rodrigues <csixteen@gmail.com>
2018-03-17 19:02:30 +0000

    Submission to Roman to Integer

---
+++ Algorithms/roman-to-integer.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        next_order = {'I': ['V', 'X'], 'X': ['L', 'C'], 'C': ['D', 'M']}
+
+        i, total = 0, 0
+        previous_c = None
+        while i < len(s):
+            if i < len(s)-1 and \
+                    (s[i] in next_order) and \
+                    (s[i+1] in next_order[s[i]]):
+                if previous_c != s[i]:
+                    tmp = values[s[i+1]] - values[s[i]]
+                    step = 2
+            else:
+                tmp = values[s[i]]
+                step = 1
+            total += tmp
+            i += step
+        return total
+
+
+class TestSolution(unittest.TestCase):
+    def test_romanToInt(self):
+        s = Solution()
+        self.assertEquals(s.romanToInt('IX'), 9)
+        self.assertEquals(s.romanToInt('MCDXCIX'), 1499)
+        self.assertEquals(s.romanToInt('III'), 3)
+        self.assertEquals(s.romanToInt('XIX'), 19)
+        self.assertEquals(s.romanToInt('MIII'), 1003)
+        self.assertEquals(s.romanToInt('MDCCCLXXXIV'), 1884)
+
+
+if __name__ == '__main__':
+    unittest.main()
