e74d92e75087a0c2cc98c58d6d7394ffc8f5baa0
Pedro Rodrigues <csixteen@gmail.com>
2018-03-23 09:10:08 +0000

    Submission to Valid Parentheses

---
+++ Algorithms/valid-parentheses.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import unittest
+
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pairs = {'(': ')', '{': '}', '[': ']'}
+        opening = []
+        count = 0
+        for c in s:
+            if c in pairs:
+                opening.append(c)
+                count += 1
+            elif len(opening) == 0:
+                return False
+            else:
+                o = opening.pop()
+                if c != pairs[o]:
+                    return False
+                count -= 1
+        return count == 0
+
+
+class TestSolution(unittest.TestCase):
+    def test_isValid(self):
+        s = Solution()
+        self.assertFalse(s.isValid('['))
+        self.assertFalse(s.isValid('(('))
+        self.assertTrue(s.isValid('()'))
+        self.assertTrue(s.isValid('()[]{}'))
+        self.assertFalse(s.isValid('(]'))
+        self.assertFalse(s.isValid('([)]'))
+        self.assertTrue(s.isValid('(()((())))'))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
