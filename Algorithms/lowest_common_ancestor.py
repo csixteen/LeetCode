dd4caf094df19bb800a228e5486b7fa72e47f563
Pedro Rodrigues <csixteen@protonmail.com>
2019-11-11 08:17:41 +0000

    Lowest Common Ancestir in BT

---
+++ Algorithms/lowest_common_ancestor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+import unittest
+
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root:
+            return None
+
+        if root.val == p.val or root.val == q.val:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        elif left:
+            return left
+        else:
+            return right
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.test_cases = []
+
+    def test_lowestCommonAncestor(self):
+        s = Solution()
+        for i, e in self.test_cases:
+            self.assertEqual(s.lowestCommonAncestor(*i), e)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
