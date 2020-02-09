beb5fa335a91f8dfae572a63c598e09af4c4be3d
Pedro Rodrigues <csixteen@protonmail.com>
2020-02-09 10:23:42 +0000

    House keeping

--- find_all_duplicates_in_array.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def findDuplicates(self, nums):
-        dups, numset = [], set()
-        for e in nums:
-            if e in numset:
-                dups.append(e)
-            else:
-                numset.add(e)
-        return dups
