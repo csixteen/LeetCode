c866cc903ba8ba6c8d31ca846ad079f12ca5cb3e
Pedro Rodrigues <csixteen@protonmail.com>
2020-03-30 15:20:29 +0100

    First unique character

---
+++ Algorithms/first-unique-char.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# https://leetcode.com/problems/first-unique-character-in-a-string/
+
+from array import array
+from collections import Counter
+import unittest
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counts = Counter(s)
+        for i, c in enumerate(s):
+            if counts[c] == 1:
+                return i
+
+        return -1
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.input_expected = [
+            (
+                {"s": "leetcode"},
+                0,
+            ),
+            (
+                {"s": "loveleetcode"},
+                2,
+            ),
+        ]
+
+    def test_firstUniqChar(self):
+        s = Solution()
+
+        for _input, expected in self.input_expected:
+            self.assertEqual(expected, s.firstUniqChar(**_input))
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
