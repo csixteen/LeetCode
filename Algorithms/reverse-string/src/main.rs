c8656ec3fb5f3329f76e13f6100183b6cfb1599b
Pedro Rodrigues <csixteen@protonmail.com>
2020-03-31 22:13:37 +0100

    Reverse string

---
+++ Algorithms/reverse-string/src/main.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        if s.len() == 0 {
+            return ();
+        }
+        
+        let mut i = 0;
+        let mut j = s.len() - 1;
+        
+        while i < j {
+            s.swap(i, j);
+            i += 1;
+            j -= 1;
+        }
+    }
+}
