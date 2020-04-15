ef42fa5424186b58438d16e34227460b935bb2f3
Pedro Rodrigues <csixteen@protonmail.com>
2020-04-15 16:07:02 +0100

    Product of array except self

--- Algorithms/product-of-array-except-self.py
+++ Algorithms/product-of-array-except-self.py
@@ -15,7 +15,7 @@ class Solution:
         for i in range(1, len(nums)):
             left[i] = left[i-1] * nums[i-1]
 
-        for i in range(len(nums) - 2, -1, -1):
+        for i in reversed(range(len(nums) - 1)):
             right[i] = right[i+1] * nums[i+1]
 
         return list(starmap(int.__mul__, zip(left, right)))
