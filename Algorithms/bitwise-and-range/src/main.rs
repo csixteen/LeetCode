bfe8f2999f1f6602f89eded3b3c009c96a004c0d
Pedro Rodrigues <csixteen@protonmail.com>
2020-04-23 09:37:29 +0100

    Bitwise AND of numbers range

---
+++ Algorithms/bitwise-and-range/src/main.rs
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/bitwise-and-of-numbers-range/
+
+impl Solution {
+    // Brute-force solution (accepted)
+    pub fn range_bitwise_and(m: i32, n: i32) -> i32 {
+        (m..=n).fold(i32::max_value(), |acc, x| acc & x)
+    }
+}
