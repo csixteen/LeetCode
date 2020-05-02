34d8225d2d33666f677a3ca3b9fc9d6aabac1eb7
Pedro Rodrigues <csixteen@protonmail.com>
2020-05-02 13:13:20 +0100

    Jewels and Stones

---
+++ Algorithms/jewels-and-stones/src/lib.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+struct Solution {}
+
+impl Solution {
+    pub fn num_jewels_in_stones(j: String, s: String) -> i32 {
+        let jewels: HashSet<char> = HashSet::from_iter(j.chars());
+
+        s
+            .chars()
+            .filter(|x| jewels.contains(x))
+            .count() as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase {
+        j: String,
+        s: String,
+        expected: i32,
+    }
+
+    #[test]
+    fn test_num_jewels_in_stones() {
+        let cases = [
+            TestCase { j: String::from("aA"), s: String::from("aAAbbbb"), expected: 3 },
+            TestCase { j: String::from("z"), s: String::from("ZZ"), expected: 0 },
+        ];
+
+        for case in cases.iter() {
+            assert_eq!(
+                case.expected,
+                Solution::num_jewels_in_stones(case.j.clone(), case.s.clone()),
+            );
+        }
+    }
+}
