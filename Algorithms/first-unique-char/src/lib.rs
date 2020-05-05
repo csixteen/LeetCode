7fbc89f67907e13f4cd123ec3cab192cf577021b
Pedro Rodrigues <csixteen@protonmail.com>
2020-05-05 18:13:55 +0100

    First unique character in string revisited

---
+++ Algorithms/first-unique-char/src/lib.rs
@@ -0,0 +1,55 @@
+struct Solution {}
+
+impl Solution {
+    pub fn first_uniq_char(s: String) -> i32 {
+        let char_freq = s
+            .bytes()
+            .fold([0; 26], |mut acc, c| {
+                acc[(c - b'a') as usize] += 1;
+                acc
+            });
+
+        for (i, c) in s.bytes().enumerate() {
+            if char_freq[(c - b'a') as usize] == 1 {
+                return i as i32;
+            }
+        }
+
+        -1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase {
+        s: String,
+        expected: i32,
+    }
+
+    #[test]
+    fn test_first_uniq_char() {
+        let test_cases = [
+            TestCase {
+                s: String::from("leetcode"),
+                expected: 0,
+            },
+            TestCase {
+                s: String::from("loveleetcode"),
+                expected: 2,
+            },
+            TestCase {
+                s: String::from("abcdabcd"),
+                expected: -1,
+            },
+        ];
+
+        for case in test_cases.iter() {
+            assert_eq!(
+                case.expected,
+                Solution::first_uniq_char(case.s.clone()),
+            );
+        }
+    }
+}
