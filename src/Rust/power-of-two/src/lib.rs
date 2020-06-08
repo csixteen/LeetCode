12014e0be9c53f11017dbe501af96df7508fb8cc
Pedro Rodrigues <csixteen@protonmail.com>
2020-06-08 08:15:08 +0100

    Power of Two

---
+++ src/Rust/power-of-two/src/lib.rs
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/power-of-two/
+
+struct Solution {}
+
+impl Solution {
+    pub fn is_power_of_two(n: i32) -> bool {
+        n > 0 && (n & (n -1)) == 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        assert_eq!(
+            true,
+            Solution::is_power_of_two(1),
+        );
+    }
+
+    #[test]
+    fn test_example2() {
+        assert_eq!(
+            true,
+            Solution::is_power_of_two(16),
+        );
+    }
+
+    #[test]
+    fn test_example3() {
+        assert_eq!(
+            false,
+            Solution::is_power_of_two(218),
+        );
+    }
+}
