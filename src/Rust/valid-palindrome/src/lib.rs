b6c697bad8c9ba2e42adbd427611d02bb940e633
Pedro Rodrigues <csixteen@protonmail.com>
2020-07-03 21:50:34 +0100

    Valid Palindrome

---
+++ src/Rust/valid-palindrome/src/lib.rs
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+struct Solution;
+
+impl Solution {
+    pub fn is_palindrome2(s: String) -> bool {
+        let f: Vec<char> = s
+            .to_lowercase()
+            .chars()
+            .filter(|x| x.is_alphanumeric())
+            .collect();
+
+        f == f.iter().rev().cloned().collect::<Vec<char>>()
+    }
+
+    pub fn is_palindrome3(s: String) -> bool {
+        let s: Vec<char> = s.to_lowercase().chars().filter(|x| x.is_alphanumeric()).collect();
+        let (mut lo, mut hi) = (0, s.len() - 1);
+
+        if s.is_empty() { return true; }
+
+        while lo < hi {
+            if s[lo] != s[hi] { return false; }
+            lo += 1;
+            hi -= 1;
+        }
+
+        true
+    }
+
+    pub fn is_palindrome(s: String) -> bool {
+        let mut i = s
+            .chars()
+            .filter(|x| x.is_alphanumeric())
+            .map(|x| x.to_ascii_lowercase());
+
+        i.clone().eq(i.rev())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        assert!(
+            Solution::is_palindrome(String::from("A man, a plan, a canal: Panama")),
+        );
+    }
+
+    #[test]
+    fn test_example2() {
+        assert!(
+            !Solution::is_palindrome(String::from("race a car")),
+        );
+    }
+
+    #[test]
+    fn test_example3() {
+        assert!(Solution::is_palindrome(String::new()));
+    }
+
+    #[test]
+    fn test_example4() {
+        assert!(!Solution::is_palindrome(String::from("0P")));
+    }
+}
