b0708f13bd25962f52b78ac6df62d474c23314cb
Pedro Rodrigues <csixteen@protonmail.com>
2020-07-09 20:43:47 +0100

    Reverse Linked List

---
+++ src/Rust/reverse-linked-list/src/lib.rs
@@ -0,0 +1,81 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val
+        }
+    }
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut prev = None;
+        let mut curr = head;
+
+        while let Some(mut node) = curr.take() {
+            let future = node.next.take();
+            node.next = prev.take();
+
+            prev = Some(node);
+            curr = future;
+        }
+
+        prev
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        assert_eq!(
+            Some(Box::new(ListNode {
+                val: 5,
+                next: Some(Box::new(ListNode {
+                    val: 4,
+                    next: Some(Box::new(ListNode {
+                        val: 3,
+                        next: Some(Box::new(ListNode {
+                            val: 2,
+                            next: Some(Box::new(ListNode {
+                                val: 1,
+                                next: None,
+                            })),
+                        })),
+                    })),
+                })),
+            })),
+            Solution::reverse_list(
+                Some(Box::new(ListNode {
+                    val: 1,
+                    next: Some(Box::new(ListNode {
+                        val: 2,
+                        next: Some(Box::new(ListNode {
+                            val: 3,
+                            next: Some(Box::new(ListNode {
+                                val: 4,
+                                next: Some(Box::new(ListNode {
+                                    val: 5,
+                                    next: None,
+                                })),
+                            })),
+                        })),
+                    })),
+                })),
+            ),
+        );
+    }
+}
