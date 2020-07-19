2b8841b9bcdf518498bea72fd301586595311100
Pedro Rodrigues <csixteen@protonmail.com>
2020-07-19 16:24:18 +0100

    Validate Binary Search Tree

---
+++ src/Rust/validate-binary-search-tree/src/lib.rs
@@ -0,0 +1,78 @@
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None
+        }
+    }
+}
+
+struct Solution;
+
+impl Solution {
+    fn _is_valid_bst(node: &Option<Rc<RefCell<TreeNode>>>, min: i64, max: i64) -> bool {
+        if let Some(n) = node {
+            let v = n.borrow().val as i64;
+
+            if v <= min || v >= max { return false; }
+
+            Self::_is_valid_bst(&n.borrow().left, min, v) &&
+                Self::_is_valid_bst(&n.borrow().right, v, max)
+        } else {
+            true
+        }
+    }
+
+    pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        Self::_is_valid_bst(&root, i64::MIN, i64::MAX)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        assert!(
+            Solution::is_valid_bst(
+                Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode::new(1)))),
+                    right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+                }))),
+            ),
+        );
+    }
+
+    #[test]
+    fn test_example2() {
+        assert!(
+            !Solution::is_valid_bst(
+                Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: Some(Rc::new(RefCell::new(TreeNode::new(1)))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+                        right: Some(Rc::new(RefCell::new(TreeNode::new(6)))),
+                    }))),
+                }))),
+            ),
+        );
+    }
+}
