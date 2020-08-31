7171b3759afe3e4576ce6151027102fefb211884
Pedro Rodrigues <csixteen@protonmail.com>
2020-08-31 15:39:08 +0100

    Binary Tree Preorder Traversal (Scala)

--- src/Scala/src/main/scala/trees/Solution.scala
+++ src/Scala/src/main/scala/trees/Solution.scala
@@ -9,13 +9,19 @@ class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null)
 
 
 object Solution {
-  /**
-   * https://leetcode.com/problems/binary-tree-inorder-traversal/
-   */
+  // https://leetcode.com/problems/binary-tree-inorder-traversal/
   def inorderTraversal(root: TreeNode): List[Int] = {
     root match {
       case null => List()
       case _ => inorderTraversal(root.left) ::: List(root.value) ::: inorderTraversal(root.right)
+    }
+  }
+
+  // https://leetcode.com/problems/binary-tree-preorder-traversal/
+  def preorderTraversal(root: TreeNode): List[Int] = {
+    root match {
+      case null => List()
+      case _ => List(root.value) ::: preorderTraversal(root.left) ::: preorderTraversal(root.right)
     }
   }
 }
--- src/Scala/src/test/scala/trees/SolutionTest.scala
+++ src/Scala/src/test/scala/trees/SolutionTest.scala
@@ -6,4 +6,20 @@ class SolutionTest extends AnyFunSuite {
   test("Solution.inorderTraversal empty tree") {
     assert(Solution.inorderTraversal(null) === List())
   }
+
+  test("Solution.inorderTraversal tree not empty") {
+    assert(Solution.inorderTraversal(
+      new TreeNode(1, _right = new TreeNode(2, _left = new TreeNode(3)))
+    ) === List(1, 3, 2))
+  }
+
+  test("Solution.preorderTraversal empty tree") {
+    assert(Solution.preorderTraversal(null) == List())
+  }
+
+  test("Solution.preorderTraversal tree not empty") {
+    assert(Solution.preorderTraversal(
+      new TreeNode(1, _right = new TreeNode(2, _left = new TreeNode(3)))
+    ) === List(1, 2, 3))
+  }
 }
