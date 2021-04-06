4bdb00146fd976229717be79cbcdec5729c8d0c0
Pedro Rodrigues <csixteen@protonmail.com>
2021-04-06 11:42:02 +0200

    Find Kth Missing Positive - Binary Search (Rust)

--- Algorithms/src/Rust/find-kth-positive/src/lib.rs
+++ Algorithms/src/Rust/find-kth-positive/src/lib.rs
@@ -4,6 +4,7 @@ struct Solution;
 
 impl Solution {
     pub fn find_kth_positive(arr: Vec<i32>, k: i32) -> i32 {
+        // Time complexity: O(N)
         let mut missing = 0;
         let mut previous = 0;
         let mut k = k;
@@ -24,6 +25,7 @@ impl Solution {
     }
 
     pub fn find_kth_positive_simplified(arr: Vec<i32>, k: i32) -> i32 {
+        // Time complexity: O(N)
         let mut i = 0;
         let mut k = k;
 
@@ -33,6 +35,23 @@ impl Solution {
         }
 
         k
+    }
+
+    pub fn find_kth_positive_bs(arr: Vec<i32>, k: i32) -> i32 {
+        let len = arr.len();
+        let (mut lo, mut hi) = (0, len - 1);
+
+        while lo <= hi {
+            let mid = lo + (hi - lo) / 2;
+
+            if k > arr[mid] - (mid as i32) - 1 {
+                 lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        lo as i32 + k
     }
 }
 
@@ -50,5 +69,11 @@ mod tests {
     fn find_kth_positive_simplified() {
         assert_eq!(9, Solution::find_kth_positive_simplified(vec![2, 3, 4, 7, 11], 5));
         assert_eq!(6, Solution::find_kth_positive_simplified(vec![1, 2, 3, 4], 2));
+    }
+
+    #[test]
+    fn find_kth_positive_bs() {
+        assert_eq!(9, Solution::find_kth_positive_bs(vec![2, 3, 4, 7, 11], 5));
+        assert_eq!(6, Solution::find_kth_positive_bs(vec![1, 2, 3, 4], 2));
     }
 }
