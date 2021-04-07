a40647ae9ea66cd72d1a3c9e64f14b105b077ac2
Pedro Rodrigues <csixteen@protonmail.com>
2021-04-07 18:17:10 +0200

    House keeping

--- Algorithms/src/Rust/longest-common-subsequence/src/lib.rs
+++ Algorithms/src/Rust/longest-common-subsequence/src/lib.rs
@@ -1,8 +1,10 @@
 // https://leetcode.com/problems/longest-common-subsequence/
 
+#![allow(dead_code)]
+
 use std::cmp;
 
-struct Solution {}
+struct Solution; 
 
 impl Solution {
     pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
