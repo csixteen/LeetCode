9a7d9a49f30189372dccec74327c6a5eda56a0a3
Pedro Rodrigues <csixteen@protonmail.com>
2021-07-11 20:16:36 +0200

    Toeplitz Matrix (Rust)

---
+++ Algorithms/src/Rust/toeplitz-matrix/src/lib.rs
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/toeplitz-matrix/
+
+struct Solution;
+
+impl Solution {
+    pub fn is_toeplitz_matrix(matrix: Vec<Vec<i32>>) -> bool {
+        let (rows, cols) = (matrix.len(), matrix[0].len());
+
+        for j in 0..rows {
+            for i in 0..cols {
+                if j == 0 || i == 0 { continue; }
+
+                if matrix[j][i] != matrix[j-1][i-1] {
+                    return false
+                }
+            }
+        }
+
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn example1() {
+        assert!(
+            Solution::is_toeplitz_matrix(
+                vec![
+                    vec![1, 2, 3, 4],
+                    vec![5, 1, 2, 3],
+                    vec![9, 5, 1, 2],
+                ],
+            ),
+        );
+    }
+
+    #[test]
+    fn example2() {
+        assert!(
+            !Solution::is_toeplitz_matrix(
+                vec![vec![1, 2], vec![2, 2]],
+            ),
+        );
+    }
+}
