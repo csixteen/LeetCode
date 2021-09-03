396edf616eb63d86b1777dd66652aa4771a0e6f5
Pedro Rodrigues <csixteen@protonmail.com>
2021-09-03 21:42:00 +0200

    Sorted Squares and Duplicate Zeros (Scala)

--- Explore/Scala/src/main/scala/array/Solution.scala
+++ Explore/Scala/src/main/scala/array/Solution.scala
@@ -1,6 +1,9 @@
 package array
 
+import scala.collection.mutable.Queue
+
 object Solution {
+  // https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3237/
   def findNumbers(nums: Array[Int]): Int = {
     @annotation.tailrec
     def numDigits(n: Int, acc: Int): Int = {
@@ -9,5 +12,24 @@ object Solution {
     }
 
     nums.filter(numDigits(_, 0) % 2 == 0).length
+  }
+
+  // https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/
+  def sortedSquares(nums: Array[Int]): Array[Int] = {
+    nums.map(x => x*x).sorted
+  }
+
+  // https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3245/
+  def duplicateZeros(arr: Array[Int]): Unit = {
+    def go(i: Int, q: Queue[Int]): Unit = {
+      if (i < arr.length) {
+        if (arr(i) == 0) q.enqueue(0, 0)
+        else q.enqueue(arr(i))
+        arr(i) = q.dequeue()
+        go(i+1, q)
+      }
+    }
+
+    go(0, new Queue[Int]())
   }
 }
--- Explore/Scala/src/test/scala/array/SolutionTest.scala
+++ Explore/Scala/src/test/scala/array/SolutionTest.scala
@@ -10,4 +10,27 @@ class SolutionTest extends AnyFunSuite {
     assert(findNumbers(Array(12,345,2,6,7896)) == 2)
     assert(findNumbers(Array(555,901,482,1771)) == 1)
   }
+
+  test("Squares of a Sorted Array") {
+    assert(sortedSquares(Array(-4, -1, 0, 3, 10)) === Array(0, 1, 9, 16, 100))
+    assert(sortedSquares(Array(-7, -3, 2, 3, 11)) === Array(4, 9, 9, 49, 121))
+  }
+
+  test("Duplicate Zeros") {
+    var example1 = Array(1, 0, 2, 3, 0, 4, 5, 0)
+    duplicateZeros(example1)
+    assert(example1 === Array(1, 0, 0, 2, 3, 0, 0, 4))
+
+    var example2 = Array(1, 2, 3)
+    duplicateZeros(example2)
+    assert(example2 === Array(1, 2, 3))
+
+    //var example3 = Array(0, 0, 1)
+    //duplicateZeros(example3)
+    //assert(example3 === Array(0, 0, 0))
+
+    var example4 = Array(8, 4, 5, 0, 0, 0, 0, 7)
+    duplicateZeros(example4)
+    assert(example4 === Array(8, 4, 5, 0, 0, 0, 0, 0))
+  }
 }
