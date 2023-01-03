1d401f1dd3cb22245af9616936b405bb9ab4aa2a
Pedro Rodrigues <csixteen@protonmail.com>
2023-01-03 19:35:54 +0100

    Minor tweaks

--- Problems/Algorithms/src/Rust/rotate-array/src/lib.rs
+++ Problems/Algorithms/src/Rust/rotate-array/src/lib.rs
@@ -1,9 +1,11 @@
+#![allow(dead_code)]
+
 use std::mem;
 
-struct Solution {}
+struct Solution;
 
 impl Solution {
-    pub fn rotate_ON_Space(nums: &mut Vec<i32>, k: i32) {
+    pub fn rotate_linear_space(nums: &mut Vec<i32>, k: i32) {
         let len = nums.len();
         let k = k as usize % len;
         if len < 2 || k == 0 { return () }
@@ -18,10 +20,10 @@ impl Solution {
             tmp.push(nums[i]);
         }
 
-        mem::replace(nums, tmp);
+        let _ = mem::replace(nums, tmp);
     }
 
-    pub fn rotate_O1_Space(nums: &mut Vec<i32>, k: i32) {
+    pub fn rotate_constant_space(nums: &mut Vec<i32>, k: i32) {
         let len = nums.len();
         let k = k as usize % len;
         nums.reverse();
@@ -37,42 +39,42 @@ mod tests {
     #[test]
     fn test_example1() {
         let mut v = vec![1, 2, 3, 4, 5, 6, 7];
-        Solution::rotate_O1_Space(&mut v, 3);
+        Solution::rotate_constant_space(&mut v, 3);
         assert_eq!(vec![5, 6, 7, 1, 2, 3, 4], v);
     }
 
     #[test]
     fn test_example2() {
         let mut v = vec![-1, -100, 3, 99];
-        Solution::rotate_O1_Space(&mut v, 2);
+        Solution::rotate_constant_space(&mut v, 2);
         assert_eq!(vec![3, 99, -1, -100], v);
     }
 
     #[test]
     fn test_example3() {
         let mut v = vec![-1, -100, 3, 99];
-        Solution::rotate_O1_Space(&mut v, 3);
+        Solution::rotate_constant_space(&mut v, 3);
         assert_eq!(vec![-100, 3, 99, -1], v);
     }
 
     #[test]
     fn test_example4() {
         let mut v = vec![-1, -100, 3, 99, 5];
-        Solution::rotate_O1_Space(&mut v, 2);
+        Solution::rotate_constant_space(&mut v, 2);
         assert_eq!(vec![99, 5, -1, -100, 3], v);
     }
 
     #[test]
     fn test_example5() {
         let mut v = vec![1];
-        Solution::rotate_O1_Space(&mut v, 1);
+        Solution::rotate_constant_space(&mut v, 1);
         assert_eq!(vec![1], v);
     }
 
     #[test]
     fn test_example6() {
         let mut v = vec![1, 2];
-        Solution::rotate_O1_Space(&mut v, 2);
+        Solution::rotate_constant_space(&mut v, 2);
         assert_eq!(vec![1, 2], v);
     }
 }
