a2649f15b22310b8ee6154134dc6ba33932d47c9
Pedro Rodrigues <csixteen@protonmail.com>
2025-01-18 14:56:01 +0100

    Valid parentheses

--- Problems/Algorithms/src/Rust/valid-parentheses/src/lib.rs
+++ Problems/Algorithms/src/Rust/valid-parentheses/src/lib.rs
@@ -6,13 +6,7 @@ struct Solution;
 
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        fn opening(c: char) -> bool {
-            match c {
-                '(' | '[' | '{' => true,
-                _ => false,
-            }
-        }
-
+        #[inline]
         fn matching(c: char) -> char {
             match c {
                 ')' => '(',
@@ -23,13 +17,12 @@ impl Solution {
         }
 
         let mut stack = Vec::new();
-        
+
         for c in s.chars() {
-            match opening(c) {
-                true => stack.push(c),
-                false if stack.is_empty() => return false,
-                false if stack[stack.len() - 1] != matching(c) => return false,
-                _ => { stack.pop(); },
+            if c == '(' || c == '[' || c == '{' {
+                stack.push(c);
+            } else if Some(matching(c)) != stack.pop() {
+                return false;
             }
         }
 
@@ -43,43 +36,31 @@ mod tests {
 
     #[test]
     fn test_example1() {
-        assert!(
-            Solution::is_valid(String::from("()")),
-        );
+        assert!(Solution::is_valid(String::from("()")),);
     }
 
     #[test]
     fn test_example2() {
-        assert!(
-            Solution::is_valid(String::from("()[]{}")),
-        );
+        assert!(Solution::is_valid(String::from("()[]{}")),);
     }
 
     #[test]
     fn test_example3() {
-        assert!(
-            !Solution::is_valid(String::from("(]")),
-        );
+        assert!(!Solution::is_valid(String::from("(]")),);
     }
 
     #[test]
     fn test_example4() {
-        assert!(
-            !Solution::is_valid(String::from("([)]")),
-        );
+        assert!(!Solution::is_valid(String::from("([)]")),);
     }
 
     #[test]
     fn test_example5() {
-        assert!(
-            Solution::is_valid(String::from("{[]}")),
-        );
+        assert!(Solution::is_valid(String::from("{[]}")),);
     }
 
     #[test]
     fn test_example6() {
-        assert!(
-            !Solution::is_valid(String::from("(")),
-        );
+        assert!(!Solution::is_valid(String::from("(")),);
     }
 }
